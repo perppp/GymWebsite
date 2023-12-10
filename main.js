5dbb375dc10bdb8246c25e73b4166815766313ae
perppp <153451320+perppp@users.noreply.github.com>
2023-12-10 21:55:22 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// let currentSlide = 0;
+// const slides = document.querySelectorAll('.slide');
+
+// function nextSlide() {
+//   slides[currentSlide].style.transform = 'translateX(-100%)';
+  
+//   currentSlide = (currentSlide + 1) % slides.length;
+  
+//   slides[currentSlide].style.transform = 'translateX(0)';
+// }
+
+// // Initialize the first slide
+// slides[currentSlide].style.transform = 'translateX(0)';
+
+// setInterval(nextSlide, 3000);
