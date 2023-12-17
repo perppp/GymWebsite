6fe9bfe9873261fc04c121253dec1a16d5bfa33e
perppp <Jugrondi@Gmail.Com>
2023-12-17 20:18:04 +0400

    Bike Thing

--- main.js
+++ main.js
@@ -13,3 +13,23 @@
 // slides[currentSlide].style.transform = 'translateX(0)';
 
 // setInterval(nextSlide, 3000);
+
+var info1Elements = document.querySelectorAll('.info1');
+
+  info1Elements.forEach(function(info1Element) {
+    var info1PathElements = info1Element.querySelectorAll('path');
+
+    info1Element.addEventListener('mouseover', function() {
+      // Change the color of info1 path to "#fc540c"
+      info1PathElements.forEach(function(pathElement) {
+        pathElement.style.fill = '#fc540c';
+      });
+    });
+
+    info1Element.addEventListener('mouseout', function() {
+      // Restore the original color when mouse leaves
+      info1PathElements.forEach(function(pathElement) {
+        pathElement.style.fill = '#676a79';
+      });
+    });
+  });
